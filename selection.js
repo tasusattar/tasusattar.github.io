f1d826dd300618306b724dfc0aff84376b287325
Tasawar Sattar <tasawar.sattar@mail.utoronto.ca>
2020-02-03 19:50:40 -0500

    genereate

--- selection.js
+++ selection.js
@@ -8,5 +8,5 @@ for (var divi = 0; divi < divs.length; divi++){
   var div = divs[divi];
   var realelem = document.getElementById(div);
   thispg.unhide(div, realelem);
-  thispg.genereate(div, realelem);
+  thispg.generate(div, realelem);
 }
