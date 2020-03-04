fcd78f1d652f1ce398e2d7f8cdf3880bd0e78fde
Tasawar Sattar <tasawar.sattar@mail.utoronto.ca>
2020-03-03 22:06:53 -0500

    new bg

--- HOME/home.js
+++ HOME/home.js
@@ -1,6 +1,8 @@
 var prntwindow = window.parent;
 var backbutt = prntwindow.backbutt;
 var prevpgstack = prntwindow.prevpgstack;
+var frameel = prntwindow.frameelem;
 
 backbutt.style.display = 'none';
+frameel.style.backgroundColor = '';
 prevpgstack = [];
--- spoof.js
+++ spoof.js
@@ -7,10 +7,11 @@ directpg = {
   'PORTFOLIO': null
 };
 
-symbol = document.getElementById('symbl');
+// symbol = document.getElementById('symbl');
 backbutt = document.getElementById('bac');
 leftbutt = document.getElementById('left');
 rightbutt = document.getElementById('right');
+frameelem = document.getElementById('bod');
 
 
 // Make Pages and store in directpg
@@ -30,13 +31,15 @@ current.loadpage();
 var choosefirstpage = function(id){
   // document.body.scrollTop = 0;
   // document.documentElement.scrollTop = 0;
+  // frameelem.setAttribute('style', 'background-color: #fffff4');
+  backbutt.style.backgroundColor = '#fffff4';
   backbutt.style.display = 'block';
   var pgofchoice = directpg[id];
   loadbod(pgofchoice);
 };
 
 var loadbod = function(page){
-  hideall();
+  // hideall();
   prevpgstack.push(current);
   current = page;
   current.loadpage();
@@ -44,9 +47,8 @@ var loadbod = function(page){
 };
 
 var hideall = function(){
-  var frameelem = document.getElementById('bod');
-  var iframeDocument = frameelem.contentWindow.document;
-  // frameelem.contentDocument ||
+  // var frameelem = document.getElementById('bod');
+  var iframeDocument = frameelem.contentWindow.document || frameelem.contentDocument;
   var allkids = iframeDocument.body.children;
   for (var kidi = 0; kidi < allkids.length; kidi++){
     allkids[kidi].style.display = 'none';
