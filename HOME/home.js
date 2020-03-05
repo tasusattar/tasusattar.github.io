c5335416da09261829c0ecd008d101fdda685ba1
Tasawar Sattar <tasawar.sattar@mail.utoronto.ca>
2020-03-05 14:22:04 -0500

    with height

--- HOME/home.js
+++ HOME/home.js
@@ -6,3 +6,8 @@ var frameel = prntwindow.frameelem;
 backbutt.style.display = 'none';
 frameel.style.backgroundColor = 'transparent';
 prevpgstack = [];
+
+frameel.style.height = "94vh"
+frameel.style.visibility = "visible";
+frameel.style.opacity = 1;
+// frameel.style.top = 'auto';
