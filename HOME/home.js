d5990c225309ff4c450e700ea23c1ce9a4f56f64
Tasawar Sattar <tasawar.sattar@mail.utoronto.ca>
2020-03-11 22:59:02 -0400

    lets give it a go

--- HOME/home.js
+++ HOME/home.js
@@ -2,6 +2,8 @@ var prntwindow = window.parent;
 var backbutt = prntwindow.backbutt;
 var prevpgstack = prntwindow.prevpgstack;
 var frameel = prntwindow.frameelem;
+var slider = document.getElementById('slideshow');
+var openingpics = ['OpeningPhotos/ColorCover.JPG', 'OpeningPhotos/Squirrel.jpg'];
 
 backbutt.style.display = 'none';
 frameel.style.backgroundColor = 'transparent';
@@ -28,4 +30,32 @@ setTimeout(function(){
   frameel.style.width = "92vw";
   frameel.style.visibility = "visible";
   frameel.style.opacity = 1;
-}, 600);
+}, 400);
+
+var slideanimate = funciton(){
+  slider.style.visibility = "hidden";
+  slider.style.opacity = 0;
+  slider.style.margin = 100%;
+
+  slidingmotion = setInterval(function(){
+    slider.style.margin -= 5%;
+  }, 50);
+
+  setTimeout(function(){
+    clearInterval(slidingmotion);
+  }, 1000);
+};
+
+var i = 0;
+var slide = function(){
+  slideanimate();
+  slider.setAttribute('style', "background-image: url("+openingpics[i]+")");
+  i = (i+1 == openingpics.length) ? 0 : i+1;
+  setInterval(slide(), 3000);
+  slider.style.opacity = 1;
+  slider.style.visibility = "visible";
+};
+
+slide();
+
+// setInterval(slide());
